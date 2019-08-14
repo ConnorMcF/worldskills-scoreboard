d2cc425215f212bd227de4a13508296fb0f361b9
Connor McFarlane <cm@semtex.net>
2019-08-14 16:43:55 +0100

    Disable demo mode

--- server/assets/script.js
+++ server/assets/script.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-	demoMode: true
+	demoMode: false
 }
 
 const COUNTRIES = {"at":"Austria","by":"Belarus","br":"Brazil","ca":"Canada","cn":"China","tpe":"Chinese Taipei","co":"Colombia","cr":"Costa Rica","hr":"Croatia","eg":"Egypt","ee":"Estonia","fr":"France","de":"Germany","hu":"Hungary","id":"Indonesia","ir":"Iran","jp":"Japan","kz":"Kazakhstan","kr":"Korea","li":"Liechtenstein","mo":"Macao, China","om":"Oman","pt":"Portugal","ru":"Russia","sg":"Singapore","za":"South Africa","es":"Spain","se":"Sweden","ch":"Switzerland"}
